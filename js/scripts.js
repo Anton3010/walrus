602b2c55aae2bbde4c1bfd2cd8055508c91474a7
Anton Sharykin and Jamila Aliyeva <jamila@>
2019-10-22 11:16:30 -0700

    practice toggle using JS.

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+
+
+  $(".clickable").click(function() {
+    $("#hide").slideToggle();
+    $("#show").slideToggle();
+    $("#display").slideToggle();
+    $("#hidden").slideToggle();
+    $("#expose").slideToggle();
+  });
+
+});
+
+
+
+
+
+
+
+fadeIn()
+fadeOut()
+fadeToggle()
+slideDown()
+slideUp()
+slideToggle()
